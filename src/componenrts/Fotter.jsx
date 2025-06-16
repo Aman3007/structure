3d3b521a2e42b1a724d88dc5ce805c2614ea3287
Aman3007 <amanchauhan30072002@gmail.com>
2025-06-16 03:37:15 -0500

    Update Fotter.jsx

--- src/componenrts/Fotter.jsx
+++ src/componenrts/Fotter.jsx
@@ -1,5 +1,24 @@
 export const Footer=()=>{
 return(<>
-    <h1>Footer</h1>
+<div id="footer">
+<div>
+                    <p>Your one-stop destination for the latest in style,</p>
+                    <p>comfort, and performance footwear.</p>
+</div>
+<div>
+    
+                    <p> Email: support@stepwithus.com</p>
+                    <p> Phone: +91 9863423453</p>
+
+                    <p>Address: 432 Sneaker Street, Fashion City, karol bagh</p>
+</div>
+<div>            
+
+                    <p><a href="/"> FAQs</a></p>
+
+                    <p><a href="/"> Privacy Policy</a></p>
+</div>
+</div>
+   
 </>)
 }
