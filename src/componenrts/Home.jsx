8dce2a65ec56eba681bfd810d4282b636f156ff4
Aman3007 <amanchauhan30072002@gmail.com>
2025-06-16 03:32:01 -0500

    Update Home.jsx

--- src/componenrts/Home.jsx
+++ src/componenrts/Home.jsx
@@ -1,34 +1,35 @@
-import { useEffect, useState } from "react";
+
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
 export const Home=()=>{
-     const [data, setData] = useState([]);
- 
-  
-useEffect(() => {
-  axios
-    .get("http://localhost:5000/api/data")
-    .then((res) => {
-      setData(res.data.message);
-    })
-    .catch((err) => {
-      console.error("Error fetching data:", err);
-    });
-}, []);
-     const navigate = useNavigate();
+    const navigate = useNavigate();
 
   const handleClick = () => {
-    navigate("/about");  
+    navigate("/register");  
   };
 
-
 return(<>
+<div className="middle-part">
+  <div class="flip-card">
+    <div class="flip-card-inner">
+        <div class="flip-card-front">
+            <h2 class="title">Privacy Guaranteed</h2>
+            <p>Hover Me</p>
+        </div>
+        <div class="flip-card-back">
+            <h2 class="title">Welcome to the Whishper</h2>
+            <p> This is the place Where you share your secret without any fear of being judged by the other's</p>
+       
 
+<button onClick={handleClick}> <span>Register</span>
+</button>
 
+        
+        </div>
+    </div>
+</div>
+     
+       
+</div>
 
-    <h1>this is home</h1>
-    <h1>React + Express</h1>
-      <p>{data[0]}</p>
-       <button onClick={handleClick}>Go to About</button>
 </>)
 }
